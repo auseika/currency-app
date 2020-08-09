bcc1e87f372a45a050f546418d2de66798da1ad8
Kanstantsin Auseika <kauseika@i-deasoft.com>
2020-08-09 06:34:15 +0300

    feat: basic navigation

--- App/App.tsx
+++ App/App.tsx
@@ -8,16 +8,36 @@ import React from 'react';
 import { enableScreens } from 'react-native-screens';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import Home from '@Screens/Home';
+import Currencies from '@Screens/Currencies';
+import CurrencyDetails from '@Screens/CurrencyDetails';
 
 // perf stuff goes here
 enableScreens();
 
 const App: () => React.ReactNode = () => {
+    // currencies stack
+    const CurrenciesStack = createStackNavigator();
+
+    function CurrenciesStackScreen() {
+        return (
+            <CurrenciesStack.Navigator>
+                <CurrenciesStack.Screen name="Currencies" component={Currencies} />
+                <CurrenciesStack.Screen name="CurrencyDetails" component={CurrencyDetails} />
+            </CurrenciesStack.Navigator>
+        );
+    }
+
+    const Tab = createBottomTabNavigator();
+
     return (
         <NavigationContainer>
-            {/* <Stack.Navigator headerMode={'none'}> */}
-            {/* <Stack.Screen name="Home" component={Home} /> */}
-            {/* </Stack.Navigator> */}
+            <Tab.Navigator>
+                <Tab.Screen name="Home" component={Home} />
+                <Tab.Screen name="Currencies" component={CurrenciesStackScreen} />
+            </Tab.Navigator>
         </NavigationContainer>
     );
 };
