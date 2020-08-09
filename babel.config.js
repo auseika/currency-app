689a0289d81e56083f8dab88a0024cb8b8107725
Kanstantsin Auseika <kauseika@i-deasoft.com>
2020-08-09 07:31:46 +0300

    feat: config decorators

--- babel.config.js
+++ babel.config.js
@@ -1,44 +1,51 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        extensions: [
-          '.js',
-          '.jsx',
-          '.ts',
-          '.tsx',
-          '.png',
-          '.jpg',
-          '.svg',
-          '.android.js',
-          '.android.tsx',
-          '.ios.js',
-          '.ios.tsx',
-          '.mp4',
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: [
+        [
+            'module-resolver',
+            {
+                extensions: [
+                    '.js',
+                    '.jsx',
+                    '.ts',
+                    '.tsx',
+                    '.png',
+                    '.jpg',
+                    '.svg',
+                    '.android.js',
+                    '.android.tsx',
+                    '.ios.js',
+                    '.ios.tsx',
+                    '.mp4',
+                ],
+                root: ['./App'],
+                alias: {
+                    '@Assets': './App/Assets',
+                    '@Components': './App/Components',
+                    '@Screens': './App/Screens',
+                    '@Types': './App/Types/index.ts',
+                    '@Globals': './App/Globals/index.ts',
+                    '@API': './App/API/index.ts',
+                    '@Stores': './App/Stores',
+                },
+            },
         ],
-        root: ['./App'],
-        alias: {
-          '@Assets': './App/Assets',
-          '@Components': './App/Components',
-          '@Screens': './App/Screens',
-          '@Types': './App/Types/index.ts',
-          '@Globals': './App/Globals/index.ts',
-          '@API': './App/API/index.ts',
-        },
-      },
+        [
+            'dotenv-import',
+            {
+                moduleName: '@env',
+                path: '.env',
+                blacklist: null,
+                whitelist: null,
+                safe: false,
+                allowUndefined: false,
+            },
+        ],
+        [
+            '@babel/plugin-proposal-decorators',
+            {
+                legacy: true,
+            },
+        ],
     ],
-    [
-      'dotenv-import',
-      {
-        moduleName: '@env',
-        path: '.env',
-        blacklist: null,
-        whitelist: null,
-        safe: false,
-        allowUndefined: false,
-      },
-    ],
-  ],
 };
